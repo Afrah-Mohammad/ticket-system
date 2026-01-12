fdd5a1b8fb100470c7c49a36a057116888465efd
Afrah-Mohammad <01fe23bcs200@kletech.ac.in>
2026-01-12 17:38:15 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use("/api/tickets", require("./routes/tickets"));
+
+app.get("/", (req, res) => {
+    res.send("Helpdesk Backend Running");
+});
+
+app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+);
